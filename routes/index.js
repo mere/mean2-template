ff8ebc8607e45b806cee8ef6b6242975f100de04
davemcmanus <dave.mcmanus@comcast.net>
2016-06-25 21:21:48 -0400

    Test commit

--- routes/index.js
+++ routes/index.js
@@ -5,8 +5,9 @@ var router = express.Router();
 //router.use('/routepath', require('./path/to/route'));
 
 // Homepage route
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'MEAN2' });
+router.get('/', function (req, res, next) {
+    res.render('index', {title: 'MEAN2'});
 });
 
 module.exports = router;
+
