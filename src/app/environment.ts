0f531e04bc81b12a48cdd3a108452f3211b9a39e
dmcmanus <dave.mcmanus@comcast.net>
2016-06-30 13:24:54 -0400

    Removed environment.ts from source control.

--- src/app/environment.ts
+++
@@ -1,9 +0,0 @@
-// CLIENT-SIDE CONFIGURATION: Development
-// 
-// These settings are copied to 'src/app/environment.ts' from 'config/client/environment.dev.ts' 
-// during a development build.
-
-export const environment = {
-    production: false,
-    apiUrl: '/api/'
-};
